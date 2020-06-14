d40fdb7117e62104f285e8a47a7ae736b7b4ef80
Vesa Hagström <weeezes@gmail.com>
2020-06-14 22:33:49 +0300

    Add build action

--- default.nix
+++ default.nix
@@ -1,2 +1,9 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./weeezes-pages.nix { }
+let
+  nixpkgs-pinned = import (builtins.fetchTarball {
+    name = "nixos-20.09pre226442";
+    url = "https://github.com/nixos/nixpkgs/archive/6405edf2dca7f6faaa29266136dfa7f8f969b511.tar.gz";
+    sha256 = "069skn0ayxmhdlw1xcj92cij7wydkk2bndkcyk4vvhms16p9wj46";
+  }) {};
+in
+  nixpkgs-pinned.pkgs.haskell.packages.${compiler}.callPackage ./weeezes-pages.nix { }
--- weeezes-pages.nix
+++ weeezes-pages.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hakyll, stdenv, text }:
+{ mkDerivation, base, file-embed, hakyll, stdenv, text }:
 mkDerivation {
   pname = "weeezes-pages";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hakyll text ];
+  executableHaskellDepends = [ base file-embed hakyll text ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
